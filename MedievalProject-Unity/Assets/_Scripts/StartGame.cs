dc307e64c13775532cc64e79b0e07f97a6b6d280
hertz30 <hzeng30@gmail.com>
2023-12-08 00:57:11 -0600

    merge and map

---
+++ MedievalProject-Unity/Assets/_Scripts/StartGame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private GameObject titleScreen;
+    [SerializeField] private GameObject aboutPage;
+    [SerializeField] private GameObject settingsPage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        titleScreen.SetActive(true);
+        aboutPage.SetActive(false);
+        settingsPage.SetActive(false);
+    }
+
+    public void PlayGame() // upon hitting the play button: ~TW
+    {
+        SceneManager.LoadScene("Game"); // load the first level. ~TW
+        Time.timeScale = 1f; // make sure time is unfrozen incase it was before. ~TW
+    }
+    public void AboutPage() // switch to instructions ~TW
+    {
+        titleScreen.SetActive(false);
+        aboutPage.SetActive(true);
+        settingsPage.SetActive(false);
+    }
+    public void SettingsPage() // switch to settings ~TW
+    {
+        titleScreen.SetActive(false);
+        aboutPage.SetActive(false);
+        settingsPage.SetActive(true);
+    }
+    public void BackToMain() // switch to main ~TW
+    {
+        titleScreen.SetActive(true);
+        aboutPage.SetActive(false);
+        settingsPage.SetActive(false);
+    }
+}
